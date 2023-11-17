c80d722208619383aa461b4af357b6f0befa472a
sudarshan <sudarshankillekar1998@gmail.com>
2023-11-17 23:30:42 +0530

    problems

---
+++ ProblemSolvings/src/Problems/findTheLargestElementIntheArray.java
@@ -0,0 +1,30 @@
+package Problems;
+import java.util.Scanner;
+public class findTheLargestElementIntheArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[] = new int [5];
+		int max;
+		
+		Scanner sc = new Scanner(System.in); 
+		System.out.println("enter any 5 element");
+		
+		for (int i = 0 ; i<5 ; i++) {
+			a[i] = sc.nextInt();
+		}
+		
+		max = a[0];
+		
+		for (int i = 1; i<5; i++) {
+			if(a[i] > max) {
+				
+				max = a[i];
+			}
+			
+					
+		}
+		System.out.println(max+ "Is the heighest element in the array");
+	}
+
+}
---
+++ ProblemSolvings/src/Problems/reversestring.java
@@ -0,0 +1,17 @@
+package Problems;
+
+public class reversestring {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "Sudarshan";
+		String rstr = "";
+		char ch;
+
+		for (int i = 0; i < str.length(); i++) {
+			ch = str.charAt(i);
+			rstr = ch + rstr;
+		}
+		System.out.println(rstr);
+	}
+
+}
