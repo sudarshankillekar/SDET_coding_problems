d794fa87b4355f6f4abb221dde2f6f6beaf83953
sudarshan <sudarshankillekar1998@gmail.com>
2023-11-22 19:19:30 +0530

    duplicate element in an array

---
+++ ProblemSolvings/src/Problems/DuplicatesInArray.java
@@ -0,0 +1,33 @@
+package Problems;
+
+import java.util.Scanner;
+
+public class DuplicatesInArray {
+
+	public static void main(String[] args) {
+		
+		//int a[] = {2,3,4,3,5,1,2};
+		int a[] = new int[7];
+		
+		Scanner sc = new Scanner(System.in); 
+		System.out.println("enter any 7 element");
+		
+		for (int i = 0 ; i<7 ; i++) {
+			a[i] = sc.nextInt();
+		}
+		
+		for(int i = 0; i<a.length; i++) {
+			for(int j = i+1; j<a.length;j++) {
+				
+				if(a[i] == a[j]) {
+					
+					System.out.println(a[i]+ " is an duplicte element");
+				}
+				
+			}
+		}
+		
+
+	}
+
+}
