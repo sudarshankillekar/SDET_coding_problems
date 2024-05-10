388a64634448701315a74f78976a2b0364976f78
sudarshan <sudarshankillekar1998@gmail.com>
2024-05-11 01:02:52 +0530

    added fibonacci series

---
+++ ProblemSolvings/src/Problems/Fibonacciseries.java
@@ -0,0 +1,31 @@
+package Problems;
+
+public class Fibonacciseries {
+
+	
+	static void fibonacilogic (int N) {
+		
+		int num1 = 0;  int num2 = 1;
+		
+		for (int i = 0 ; i < N;i++) {			
+			int num3 = num1+num2;			
+			System.out.print(num1+ " ");			
+			num1 = num2;
+			num2 = num3;			
+		}		
+	}
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+	
+	int	N = 10;
+	
+	fibonacilogic(N);
+		
+	}
+
+}
