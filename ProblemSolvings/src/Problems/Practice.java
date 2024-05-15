24247e6635c518d8adee7616f3a96dff95ff0401
sudarshan <sudarshankillekar1998@gmail.com>
2024-05-15 22:29:53 +0530

    a

--- ProblemSolvings/src/Problems/Practice.java
+++ ProblemSolvings/src/Problems/Practice.java
@@ -6,18 +6,30 @@ public class Practice {
 
 	public static void main(String[] args) {
 		
-		String str  = "Sudarshan";
-		String Rstr = " ";
-		char ch;
-		//String
-		//@
-		for(int i = 0 ; i < str.length(); i++) {
-			ch = str.charAt(i);
-			Rstr = ch + Rstr ; 
+		String Text = "GeeksforGeeks";
+		
+		char words [] = Text.toCharArray();
+		
+		HashMap <Character ,Integer> wordcount = new  HashMap<>();
+		
+		for(Character ch : words) {
+			if(wordcount.containsKey(ch)) {
+				wordcount.put(ch, wordcount.get(ch)+1);
+			}
+			else
+			wordcount.put(ch, 1);
 		}
-		System.out.println(Rstr);
-		//
+				
+		for(Character word : wordcount.keySet()) {
+			if(wordcount.get(word)>1) {
+				System.out.println(word+" "+wordcount.get(word));
+			}
+		}
+	
+		
 	}
-        }
+       
+
+}
 		
 
