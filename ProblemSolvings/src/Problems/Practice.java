56ad92fa1d42815883b9444c23f006fff291521c
sudarshan <sudarshankillekar1998@gmail.com>
2024-06-17 22:24:41 +0530

    no looking back

--- ProblemSolvings/src/Problems/Practice.java
+++ ProblemSolvings/src/Problems/Practice.java
@@ -5,34 +5,22 @@ import java.util.*;
 public class Practice {
 
 	
-		
-	static void reversedarray(int [] arr,int start,int end) {
-		while(start<end) {
-			int temp;
-			temp = arr[start] ;
-			arr[start] =  arr[end];
-			arr[end] = temp;	
-			start++;
-			end--;
+		public static void main(String[] args) {
+			
+			String str = "sudarshan";
+			String rstr = " ";
+			char ch ;
+			
+			for(int i = 0 ; i<str.length();i++) {
+				ch = str.charAt(i);
+				rstr = ch+rstr ;
+			}
+			
+			System.out.println(rstr);
+			
+			
 			
 		}
-	}
-	static void printArray(int arr[], int size) {
-			for(int i = 0;i<size;i++) {
-				System.out.print(arr[i]+" ");
-			System.out.println();
-		}
-		}
-		public static void main(String args[]) {
-			 int arr[] = {1, 2, 3, 4, 5, 6}; 
-			 printArray(arr, 6); 
-			 reversedarray(arr, 0, 5); 
-		     System.out.print("Reversed array is \n"); 
-		     printArray(arr, 6); 
-	}
-		
-	
-       
 
 }
 		
