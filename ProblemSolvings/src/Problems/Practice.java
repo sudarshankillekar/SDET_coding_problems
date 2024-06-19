cd6042c427014fa1dd2428fe58b8c42db9fa14a9
sudarshan <sudarshankillekar1998@gmail.com>
2024-06-19 21:38:44 +0530

    added latest code

--- ProblemSolvings/src/Problems/Practice.java
+++ ProblemSolvings/src/Problems/Practice.java
@@ -5,20 +5,27 @@ import java.util.*;
 public class Practice {
 
 	
-		public static void main(String[] args) {
-	    int a[] = new int[7];		
+	
+
+		public static void main (String[] args){
+
+		int a[] = new int[7];
 		Scanner sc = new Scanner(System.in);
 		System.out.println("enter any 7 digits");
-	    
-		for(int i = 0; i<7;i++) {
-			a[i] = sc.nextInt();
+
+		for(int i =0;i<7;i++){
+		a[i] = sc.nextInt();
 		}
-		
-		//for(int i )
-		
+
+		for(int i = 0;i<a.length;i++){
+		   for(int j = i+1;j<a.length;j++){
+		     if(a[i] == a[j] ){
+		     System.out.println(a[i]+" is an duplicate element");
+		}
+		}
 		
 		}
 
+		}
 }
-		
 
