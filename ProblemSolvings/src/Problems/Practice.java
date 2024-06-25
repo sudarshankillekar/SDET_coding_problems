dbbc4136e2fa1f1d5baa992e4f853c3f50651aa1
sudarshan <sudarshankillekar1998@gmail.com>
2024-06-26 00:16:33 +0530

    test

--- ProblemSolvings/src/Problems/Practice.java
+++ ProblemSolvings/src/Problems/Practice.java
@@ -3,27 +3,25 @@ package Problems;
 import java.util.*;
 
 public class Practice {
+		
+		public static void main(String[] args){
 
-	public static void main(String[] args) {
-		
-	String text = "Hi , am sudarshan , sudarshan is rahul's friend";
-	String [] words  = text.split(" ");
-	
-	HashMap <String ,Integer> WordCounter = new HashMap <String,Integer>();
-	
-	for(String word : words) {
-		if(WordCounter.containsKey(word)) {
-			WordCounter.put(word, WordCounter.get(word)+1);
-		}
-		else {
-			WordCounter.put(word, 1);
-		}
-	}
-		
-	for(String word : WordCounter.keySet()) {
-		System.out.println("Word \"" + word + "\" occurs " + WordCounter.get(word) + " time(s)");
-	}
-	
-	}
+			System.out.println(reverse(" the sky  is blue  "));
+
+			} 
+
+			static String reverse(String s){
+
+			String [] words = s.split(" +");
+			StringBuilder sb  = new StringBuilder();
+
+			for(int i = words.length-1; i>=0;i--){
+
+			sb.append(words[i]);
+			sb.append(" ");
+
+			}
+			return sb.toString().trim();
+			}
 		
 }
