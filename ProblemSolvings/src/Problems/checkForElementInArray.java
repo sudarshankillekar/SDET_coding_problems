f4e9e0ce28946a933bbbceb498a76dad82672b0f
sudarshan <sudarshankillekar1998@gmail.com>
2024-06-28 22:40:57 +0530

    added

--- ProblemSolvings/src/Problems/checkForElementInArray.java
+++ ProblemSolvings/src/Problems/checkForElementInArray.java
@@ -3,26 +3,28 @@ package Problems;
 public class checkForElementInArray {
 	
 	public static void main(String[] args) {
-		int a []  = {20,120,10,1000,20};
-		
-		int compare_element = 10;
-		boolean flag = false;
-		
-		for(int i = 0;i < a.length;i++) {
-			if(compare_element == a[i]) {
-				System.out.println("element is at index "+i+" for "+compare_element);
-				flag = true;
-				break;
-			
-			}
-		}
-		if(flag == false) {
-			System.out.println("element is not present");
-		}
-	}
-	
-	
-	
-	
+		String s = "dad";
 
+		int i ,j,flag = 0;
+		i = 0;
+		j = s.length()-1;
+
+		while(i<j && flag == 0){
+		if(s.charAt(i)!=s.charAt(j)){
+		flag = 1;
+		break;
+		}
+		i++;
+		i--;
+		}
+
+		if(flag == 0)
+		System.out.println("is palindrome");
+		else
+		System.out.println("Not palindrome");
+	
+	
+	
+	
+	}
 }
