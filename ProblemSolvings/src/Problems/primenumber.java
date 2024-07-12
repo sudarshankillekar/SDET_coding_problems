ee57b0384836064997488c2c251a124a7a826fb3
sudarshan <sudarshankillekar1998@gmail.com>
2024-07-12 17:14:26 +0530

    added prime number problem

---
+++ ProblemSolvings/src/Problems/primenumber.java
@@ -0,0 +1,29 @@
+package Problems;
+
+public class primenumber {
+
+	public static void main(String[] args) {
+		int num = 5;
+		int count = 0;
+		
+		if(num>1) {
+			
+		for(int i = 1; i<=num ;i++) {
+			
+			if(num%i==0) 
+				count++;
+		}
+		if(count == 2) {
+			System.out.println("is a prime number");
+		}
+		else {
+			System.out.println("not an prime number");
+		}			
+		}
+		else {
+			System.out.println("Not an prime number");
+		}
+
+	}
+
+}
