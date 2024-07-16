e4f4daa83d1227ac32b6fc8870ec7eeb88869271
sudarshan <sudarshankillekar1998@gmail.com>
2024-07-16 21:27:47 +0530

    added

--- ProblemSolvings/src/Problems/Practice.java
+++ ProblemSolvings/src/Problems/Practice.java
@@ -4,21 +4,25 @@ import java.util.*;
 
 public class Practice {
 	
-	public static void main(String[] args) {
-		
-	String Str = "Sudarshan";
-	String rstr = " ";
-	char ch ;
 	
-	for(int i = 0 ; i<Str.length();i++) {
-		ch = Str.charAt(i);
-		rstr = ch + rstr;
-	}
-		System.out.println(rstr);
+	public static void main(String[] args){
 
+		int [] a = new int [5];
+		Scanner sc = new Scanner(System.in);
+		
+		for(int i = 0 ; i<a.length;i++) {
+			a[i] = sc.nextInt();
+		}
+		
+		for(int i = 0 ; i <a.length ; i++) {
+			for(int j = i+1 ; j<a.length;j++) {
+				if(a[i] == a[j]) {
+					System.out.println(a[i]+"is an duplicate element in an array");
+				}
+			}
+		}
+	
 	}
-	
-	
 }
 		
 
