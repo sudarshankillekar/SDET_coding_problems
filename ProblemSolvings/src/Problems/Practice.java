0bb98128fc4c83ac958f5808eb980e8a860ece2b
sudarshan <sudarshankillekar1998@gmail.com>
2024-07-16 20:53:22 +0530

    added

--- ProblemSolvings/src/Problems/Practice.java
+++ ProblemSolvings/src/Problems/Practice.java
@@ -4,22 +4,31 @@ import java.util.*;
 
 public class Practice {
 	
+	static boolean checkForanargram(String s1 , String s2) {
+		
+		String Str1 = s1.replaceAll("//s", "");
+		String Str2 = s2.replaceAll("//s", "");
+		
+		if(s1.length()!=s2.length()) {
+			return false;
+		}
+		else {
+			char c1[] = Str1.toLowerCase().toCharArray();
+			char c2[] = Str2.toLowerCase().toCharArray();
+			
+			Arrays.sort(c1);
+			Arrays.sort(c2);
+			return Arrays.equals(c1, c2); 
+		}
+		
+		
+	}
+	
+	
 	public static void main(String[] args) {
-		
-		int [] array = {10,20,30,40,50};
-		
-		int temp = array[0];
-		
-		for(int i = 1 ; i<array.length;i++) {
-			array[i-1] = array[i];
-		}
-		
-		array[array.length-1] = temp;
-		
-		for(int i: array) {
-			System.out.print(i+" " );
-		}
-		}
+		System.out.println(checkForanargram("sudarshan", "darshanaa"));
+	}
+	
 	
 }
 		
