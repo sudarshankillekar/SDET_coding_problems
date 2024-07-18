00cc9190ba35c93294c7d9be5ee169202018bae4
sudarshan <sudarshankillekar1998@gmail.com>
2024-07-18 09:20:39 +0530

    added

--- ProblemSolvings/src/Problems/Practice.java
+++ ProblemSolvings/src/Problems/Practice.java
@@ -7,29 +7,21 @@ public class Practice {
 	
 	public static void main(String[] args){
 
-		int num = 19;
-		int count = 0;
-		
-		if(num > 1) {
-			
-			for(int i = 1; i<=num ; i++) {
-				if(num%i == 0) 
-					count++;
-				}
-			    if(count ==2) {
-			    	System.out.println("is an prime number");
-			    }
-			    else {
-			    	System.out.println("is not a prime number");
-			    }
-			    }			
-			else {		 			
-		     System.out.println();		
-	     }
-        }
-		
+     int[] array = {10,20,30,40,50};
+     int temp = array[0];
+     
+     for(int i = 1;i<array.length;i++) {
+    	 array[i-1] = array[i];
+     }
+	
+     array[array.length-1] = temp;
+     
+     for(int i : array) {
+    	 System.out.print(i+" ");
+     }
+     
 	}
-		
+}
 		
 		
 		
