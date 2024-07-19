7176d120058dfe03efdbcfcb055599192ec15eb0
sudarshan <sudarshankillekar1998@gmail.com>
2024-07-19 10:10:24 +0530

    added

--- ProblemSolvings/src/Problems/Practice.java
+++ ProblemSolvings/src/Problems/Practice.java
@@ -5,25 +5,55 @@ import java.util.*;
 public class Practice {
 	
 	public static void main(String[] args) {
-		System.out.println(reverwords("I love Java programming. Java is fun and Java is powerful."));
-	}
-	
-	
-     static String reverwords(String s) {
 		
-	String [] words = s.split(" +");
-	
-	StringBuilder sb = new StringBuilder();
-	
-	for(int i = words.length-1 ;i>=0;i--) {
-		sb.append(words[i]);
-		sb.append(" ");
+		int [] arr = {10,20,21,22,90,76,56};
+		
+		HashMap<Integer,Boolean> map = new  HashMap<>();
+		
+		for(int val:arr) {
+			map.put(val,true);
+		}
+		
+		
+		for(int val : arr) {
+			if(map.containsKey(val-1)) {
+				map.put(val, false);
+			}
+		}
+		
+		int msp = 0;
+		int ml = 0;
+		
+		for(int val : arr) {
+			if(map.get(val) ==true) {
+			int tl = 1;
+			int tsp = val;
+			
+			while(map.containsKey(tl+tsp)) {
+				tl++;
+			}
+			
+			if(tl>ml) {
+				ml = tl ;
+			    msp = tsp;				
+			}
+		}
+	}
+			System.out.println("max len"+ml);
+			
+			for(int i = msp; i < msp+ml;i++) {
+				System.out.print(i+" ");
+			}
+			
+		}
+		
+		
 	}
 	
-	return sb.toString().trim();
-        }
 	
-}
+   
+	
+
 
 		
 		
