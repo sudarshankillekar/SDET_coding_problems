62adb3de9c870c4ca6e26211423476ddbd233915
sudarshan <sudarshankillekar1998@gmail.com>
2024-07-19 21:36:36 +0530

    added fibbonaci series

--- ProblemSolvings/src/Problems/Practice.java
+++ ProblemSolvings/src/Problems/Practice.java
@@ -4,53 +4,33 @@ import java.util.*;
 
 public class Practice {
 	
+	
+	
+	 static void fibbonacilogic(int n) {
+	
+		int num1 = 0; int num2 = 1;
+		
+		for(int i = 0 ; i<n;i++) {
+			int num3 = num1+num2;
+			System.out.print(num1+" ");
+			num1 = num2;
+			num2 = num3;
+		}
+		 
+	}
+	
+	
 	public static void main(String[] args) {
 		
-		int [] arr = {10,20,21,22,90,76,56};
-		
-		HashMap<Integer,Boolean> map = new  HashMap<>();
-		
-		for(int val:arr) {
-			map.put(val,true);
-		}
-		
-		
-		for(int val : arr) {
-			if(map.containsKey(val-1)) {
-				map.put(val, false);
-			}
-		}
-		
-		int msp = 0;
-		int ml = 0;
-		
-		for(int val : arr) {
-			if(map.get(val) ==true) {
-			int tl = 1;
-			int tsp = val;
-			
-			while(map.containsKey(tl+tsp)) {
-				tl++;
-			}
-			
-			if(tl>ml) {
-				ml = tl ;
-			    msp = tsp;				
-			}
-		}
-	}
-			System.out.println("max len"+ml);
-			
-			for(int i = msp; i < msp+ml;i++) {
-				System.out.print(i+" ");
-			}
-			
-		}
+
+    int n = 10;
+    
+    fibbonacilogic(n);
 		
 		
 	}
 	
-	
+}
    
 	
 
