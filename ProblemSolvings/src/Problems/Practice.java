3d4f37b6a0f5272c7ea454d719ce4535723be3df
sudarshan <sudarshankillekar1998@gmail.com>
2024-07-19 09:34:32 +0530

    practised

--- ProblemSolvings/src/Problems/Practice.java
+++ ProblemSolvings/src/Problems/Practice.java
@@ -5,32 +5,26 @@ import java.util.*;
 public class Practice {
 	
 	public static void main(String[] args) {
-		
-		String text = "I love Java programming. Java is fun and Java is powerful.";
-		
-		String [] words = text.split(" ");
-		
-		HashMap <String , Integer> wordcounter = new HashMap<>();
-		
-		for (String word : words) {
-			if(wordcounter.containsKey(word)) {
-				wordcounter.put(word, wordcounter.get(word)+1);
-			}
-			else {
-				wordcounter.put(word,1);
-			}
-		}
-		
-		for(String word :wordcounter.keySet()) {
-			System.out.println("the word"+word+"is getting repeated these many times "+wordcounter.get(word));
-		}
-		
-		
-		
+		System.out.println(reverwords("I love Java programming. Java is fun and Java is powerful."));
 	}
 	
-}
+	
+     static String reverwords(String s) {
 		
+	String [] words = s.split(" +");
+	
+	StringBuilder sb = new StringBuilder();
+	
+	for(int i = words.length-1 ;i>=0;i--) {
+		sb.append(words[i]);
+		sb.append(" ");
+	}
+	
+	return sb.toString().trim();
+        }
+	
+}
+
 		
 		
 	
