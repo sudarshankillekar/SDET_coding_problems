01c249ca96b6c8b366bc852d6cacf1e5a4beb6d4
sudarshan <sudarshankillekar1998@gmail.com>
2024-07-19 09:10:22 +0530

    practised word counter in a string

--- ProblemSolvings/src/Problems/Practice.java
+++ ProblemSolvings/src/Problems/Practice.java
@@ -4,24 +4,31 @@ import java.util.*;
 
 public class Practice {
 	
-	public static boolean isVowel(char ch) {
-		ch = Character.toLowerCase(ch);
-		return ch == 'a' || ch == 'e' ||ch == 'i'||ch == 'o' ||ch == 'u' ;  
+	public static void main(String[] args) {
+		
+		String text = "I love Java programming. Java is fun and Java is powerful.";
+		
+		String [] words = text.split(" ");
+		
+		HashMap <String , Integer> wordcounter = new HashMap<>();
+		
+		for (String word : words) {
+			if(wordcounter.containsKey(word)) {
+				wordcounter.put(word, wordcounter.get(word)+1);
+			}
+			else {
+				wordcounter.put(word,1);
+			}
+		}
+		
+		for(String word :wordcounter.keySet()) {
+			System.out.println("the word"+word+"is getting repeated these many times "+wordcounter.get(word));
+		}
+		
+		
+		
 	}
 	
-
-	public static void main(String[] args){
-
-	char ch = 'a';
-	
-	if(isVowel(ch)) {
-		System.out.println("is an vowel");
-	}
-	else {
-		System.out.println("is an consonont");
-	}
-     
-	}
 }
 		
 		
