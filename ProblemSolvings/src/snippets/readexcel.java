62bbbb5f83b21386a8b26de42a98ab98f2d0d1b9
sudarshan <sudarshankillekar1998@gmail.com>
2024-07-24 09:23:55 +0530

    added

---
+++ ProblemSolvings/src/snippets/readexcel.java
@@ -0,0 +1,35 @@
+package snippets;
+
+public class readexcel {
+
+	public static void main(String[] args) {
+		
+		
+		//Create an object of excel that is XSSFWorkBook 
+///		XSSFWorkBook myWorkBook = new XSSFWorkBook("add excel sheet location in the project"); 
+		//Calling  the sheet from the workbook using XSSFworkBook 
+	//	XSSFSheet mySheet = myWorkBook.getSheet("Sheet name");
+		//calling row index using sheet reference 
+	///	int LastRowIndex = mySheet.getLastRowNum();
+		//calling header of the raw
+	//	XSSFSheet headerrow  = mySheet.getRow(0);
+	///	int totalnumbOfcol = headerRow.getlastcellnum();
+		
+	///	XSSFRow row ;
+	///	XSSFCell cell;
+		
+	///	for(int rowIndex = 0 ; rowIndex <= LastRowIndex ;  rowIndex++ ) {
+	///		for(int cellIndex = 0 ; cellIndex <= totalnumbOfcol ;  cellIndex++ ) {
+	///			row = mySheet.getRow(rowIndex);
+	///			cell = row.getcell(cellIndex);
+    ///				System.out.println(cell.getStringCellValue()+"");
+			}
+		//	System.out.println("");
+		///}
+		
+		
+		
+		
+	}
+
+//}
