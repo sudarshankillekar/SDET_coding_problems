83d9b0b75d41817d20e93f428dab48e08475ecde
sudarshan <sudarshankillekar1998@gmail.com>
2024-07-26 08:42:20 +0530

    added remove special characters code

---
+++ ProblemSolvings/src/Problems/RemoveSpecialCharacters.java
@@ -0,0 +1,15 @@
+package Problems;
+
+public class RemoveSpecialCharacters {
+
+	public static void main(String[] args) {
+		
+	 String s  = "Sudarshan@!@#A$$h%^&*(info)(*&tech";
+	 
+	String S = s.replaceAll("[^a-zA-Z]","" );
+	 
+	System.out.println(S); 	
+		
+	}
+
+}
