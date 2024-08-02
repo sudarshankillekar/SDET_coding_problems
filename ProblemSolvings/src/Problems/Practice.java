315776a9a1b92109f2beeb7c15e3518c256409f1
sudarshan <sudarshankillekar1998@gmail.com>
2024-08-02 23:43:07 +0530

    added

--- ProblemSolvings/src/Problems/Practice.java
+++ ProblemSolvings/src/Problems/Practice.java
@@ -5,46 +5,32 @@ import java.util.*;
 public class Practice {
 	
 	public static void main(String[] args) {
-//
-    int [] arr = {22,99,102,21,89,33,23,111};
+  
+	int [] nums = {2,9,8,1};
+	int target = 3;
+	System.out.println(Arrays.toString(twosumlogic(nums, target)));
+		
+	}	
+	
+	static int[] twosumlogic(int[] nums ,int target) {
+	
+		HashMap<Integer,Integer>  map = new HashMap<>();
+		
+		for(int i = 0;i<nums.length;i++) {
+			int req_num = target - nums[i];
+			if(map.containsKey(req_num)) {
+				int [] arr = {map.get(req_num),i};
+				return arr;
+			}
+			map.put(nums[i], i);
+		}
+		return null;		
+	}
+	
+	
+	
+	}
 
-    HashMap<Integer,Boolean> Map = new HashMap<>();
-    
-    for(int val : arr) {
-    Map.put(val, true);
-    }
-    
-    for(int val : arr) {
-    	if(Map.containsKey(val-1)){
-    		Map.put(val, false);
-    	}
-    }
-    
-	int ml = 0;
-	int msp = 0;
-	
-	for(int val : arr) {
-		if(Map.get(val) == true) {
-			int tl = 1;
-			int tsp = val;
-		
-	while(Map.containsKey(tsp+tl)) {
-	       tl++;	
-	}
-		if(tl>ml) {
-			 ml =tl;
-			  msp=tsp;
-		}
-		}	
-	}
-		System.out.println("longest subsequent in an element "+ml);
-		for(int i = msp ;i<msp+ml;i++) {
-			System.out.print(i+" ");
-		}
-		
-	}
-	}
-	
 	
 	
 		
