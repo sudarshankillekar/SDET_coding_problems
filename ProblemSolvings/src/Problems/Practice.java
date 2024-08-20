6342e22b162d92dc4c5384e6c97d3432813af382
sudarshan <sudarshankillekar1998@gmail.com>
2024-08-20 13:23:55 +0530

    added

--- ProblemSolvings/src/Problems/Practice.java
+++ ProblemSolvings/src/Problems/Practice.java
@@ -5,23 +5,47 @@ import java.util.*;
 
 public class Practice {
 	   
-  public static void main(String[] args) {
+   public static void main(String[] args) {
 	
-	  String s = " abjnoijijdk";
-	  
-	  HashMap<Character,Integer> map = new HashMap<>();
-	  
-	  for(int i = 0; i<s.length();i++) {
-		  if(map.containsKey(s.charAt(i))) {
-			  map.put(s.charAt(i), map.get(s.charAt(i))+1);
-		  }
-		  else {
-			  map.put(s.charAt(i), 1);
-		  }
-	  }
-		  map.forEach((key,value)-> System.out.print(key+"" +value));
-	  }
-	  
+	   String s  = "abcmmnqnq";
+	   System.out.println(LongestSubString(s));
+   
+}
+   
+   
+   
+   static String LongestSubString(String s) {
+	   int start = 0;
+	   int maxStart = 0;
+	   int maxLen = 0;
+	   int maxend = 0;
+	   
+	   HashMap <Character,Integer> map = new HashMap<>();
+	   
+	   for(int end = 0;end<s.length();end++) {
+		   char c  = s.charAt(end);
+		   if(map.containsKey(c)) {
+			   if(start<=map.get(c)) {
+				   start = map.get(c)+1;
+			   }
+		   }
+		   map.put(c, end);
+	   
+	   
+	   if(end-start+1 >= maxLen) {
+		   maxLen = end-start+1;
+		   maxStart = start;
+		   maxend = end;
+	   }
+	   	   
+	   }
+	   
+	   return "maxLength "+maxLen+"subString "+s.substring(maxStart,maxend+1);
+	   
+	   
+   }
+	
+	
 }
   
 
