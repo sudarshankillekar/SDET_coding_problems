be621c307ff077a2d53111abfe2d8ed139265f8f
sudarshan <sudarshankillekar1998@gmail.com>
2024-08-21 09:22:33 +0530

    added

--- ProblemSolvings/src/Problems/Practice.java
+++ ProblemSolvings/src/Problems/Practice.java
@@ -7,45 +7,18 @@ public class Practice {
 	   
    public static void main(String[] args) {
 	
-	   String s  = "abcmmnqnq";
-	   System.out.println(LongestSubString(s));
-   
+	String str = "sudarshan";
+	String rstr = " ";
+	char ch;
+	 
+	for(int i = 0;i<str.length();i++) {
+		
+	 ch = str.charAt(i);
+	 rstr = ch+rstr;
+		
+	}
+	System.out.println(rstr);
 }
-   
-   
-   
-   static String LongestSubString(String s) {
-	   int start = 0;
-	   int maxStart = 0;
-	   int maxLen = 0;
-	   int maxend = 0;
-	   
-	   HashMap <Character,Integer> map = new HashMap<>();
-	   
-	   for(int end = 0;end<s.length();end++) {
-		   char c  = s.charAt(end);
-		   if(map.containsKey(c)) {
-			   if(start<=map.get(c)) {
-				   start = map.get(c)+1;
-			   }
-		   }
-		   map.put(c, end);
-	   
-	   
-	   if(end-start+1 >= maxLen) {
-		   maxLen = end-start+1;
-		   maxStart = start;
-		   maxend = end;
-	   }
-	   	   
-	   }
-	   
-	   return "maxLength "+maxLen+"subString "+s.substring(maxStart,maxend+1);
-	   
-	   
-   }
-	
-	
 }
   
 
