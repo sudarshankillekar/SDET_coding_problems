812f44bfe105b621ef07810b40eef1ab6112374a
sudarshan <sudarshankillekar1998@gmail.com>
2024-08-24 01:04:00 +0530

    added test

--- ProblemSolvings/src/Problems/Practice.java
+++ ProblemSolvings/src/Problems/Practice.java
@@ -7,21 +7,30 @@ public class Practice {
 	   
    public static void main(String[] args) {
 	
-	String str = "sudarshan";
-	String rstr = " ";
-	char ch;
-	 
-	for(int i = 0;i<str.length();i++) {
-		
-	 ch = str.charAt(i);
-	 rstr = ch+rstr;
-		
-	}
-	System.out.println(rstr);
-}
-}
-  
+   String  s = "sudarshan killekar";
+   
+   String [] s1 = s.split(" ");
+   
+   StringBuilder sb = new StringBuilder();
+   
+   int i = 0;
+   
+   while(i<s1.length) {
+	   if(i%2 == 0) {
+		   sb.append(s1[i]);
+	   }
+	   else
+	   {
+		   sb.append(new StringBuilder(s1[i]).reverse().toString());	   
+   }
+   sb.append(" ");
+   i++;
+   }
 
+   System.out.println(sb.toString().trim());
+   	   
+}
+}
   
   
 
