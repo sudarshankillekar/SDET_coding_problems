388332b0f32a1c2ee1da65f4004d0bd752070cbf
sudarshan <sudarshankillekar1998@gmail.com>
2024-08-26 23:14:13 +0530

    added

--- ProblemSolvings/src/Problems/Practice.java
+++ ProblemSolvings/src/Problems/Practice.java
@@ -4,15 +4,35 @@ package Problems;
 import java.util.*;
 
 public class Practice {
-	   
-   public static void main(String[] args) {
-	
-	   
+		
+  
+		   
+	public static void printpairSum(int [] arr ,int sum ,int size) {
+		
+		for(int i = 0;i<size;i++) {
+			for(int j = i+1;j<size;j++) {
+				if(arr[i]+arr[j] == sum) {
+					System.out.println("("+arr[i]+" "+arr[j]+")");
+				}
+			}
+		}
+		
+		
+		
+	}
+	 public static void main(String[] args) {
+	     
+		   int [] arr = {1,23,24,4,2,3,2};
+		   int sum = 5;
+		   int size = arr.length;
+		   printpairSum(arr,sum,size);
+		  
+		   }  
 	   
 	   
 	   
    }
-}
+
 
   
 
