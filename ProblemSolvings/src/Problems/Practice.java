71b96b664f3f463f48a28f15e7997763f19d3343
sudarshan <sudarshankillekar1998@gmail.com>
2024-08-27 22:15:16 +0530

    added

--- ProblemSolvings/src/Problems/Practice.java
+++ ProblemSolvings/src/Problems/Practice.java
@@ -8,34 +8,19 @@ public class Practice {
   
 	public static void main(String[] args) {
 	   
-	
-		
-	String text = "Hi Hi my name is sudarshan and sudarshan is best at coding";
-	String [] Splited_word = text.split(" ");
-	
-	HashMap<String,Integer> map = new HashMap<>();
-	
-	for(String word : Splited_word) {
-		if(map.containsKey(word)) {
-			map.put(word, map.get(word)+1);
-		}
-		else {
-			map.put(word, 1);
-		}
-	}	
-		for(String words : map.keySet()) {
-			System.out.println(words+" "+map.get(words));
-		}
-	
-//	
-	
-	
-	
-	
-		
+     int num1 = 0;
+     int num2 = 1;
+     
+     for(int i = 0;i<9;i++) {
+    	int num3 = num1+num2;
+    	System.out.print(num1+ " ");
+    	num1 = num2;
+    	num2 = num3;
+     }	
+    }	
 	}
   
-}
+
 
   
   
