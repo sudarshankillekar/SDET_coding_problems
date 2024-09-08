a3358d65a6a1a199319201ee07e357328f02d7e5
sudarshan <sudarshankillekar1998@gmail.com>
2024-09-08 19:32:58 +0530

    added

--- ProblemSolvings/src/Strings/Practice.java
+++ ProblemSolvings/src/Strings/Practice.java
@@ -6,35 +6,21 @@ import java.util.*;
 public class Practice {
 	  
 	public static void main(String[] args) {
-		String s = "abccrrkarrlmmn";
-        System.out.println(longestSubString(s));
+		String str =  " sudarshan is name my ";
+	   String [] splitedString = str.split(" ");
+	   List<String> reversedlist = new ArrayList<>();
+ 		for( int i=splitedString.length-1;i >= 0;i--) {
+			reversedlist.add(splitedString[i]);
+	        
+		}
+		
+		
+ 		  String reversedString = String.join(" ", reversedlist);
+ 	        System.out.println(reversedString.trim());
+		
     }
 	
-	public static String longestSubString(String s) {
-		
-	  HashMap<Character, Integer> map = new HashMap<>();
-	  int start =  0;
-	  int maxlen = 0;
-	  int maxstart = 0;
-	  int maxend = 0;
-	  
-	  for(int end  = 0 ; end<s.length(); end++) {
-		  char ch = s.charAt(end);
-		  if(map.containsKey(ch)) {
-			  if(start<=map.get(ch)) {
-				  start = map.get(ch)+1;
-			  }
-		  }
-		  map.put(ch, end);
-		  if (end - start + 1 >= maxlen) {
-              maxlen = end - start + 1;
-              maxstart = start;
-              maxend = end;
-             // //test 
-          }
-      }
-      return "Length: " + maxlen + ", Substring: " + s.substring(maxstart, maxend + 1);
-  }
+	
 	}
 	
 	
--- ProblemSolvings/src/Strings/reverseWordsinString.java
+++ ProblemSolvings/src/Strings/reverseWordsinString.java
@@ -1,7 +1,27 @@
 package Strings;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class reverseWordsinString {
- //3   
+ //3  
+//Alternate method	
+//	String str =  " sudarshan is name my ";
+//	   String [] splitedString = str.split(" ");
+//	   List<String> reversedlist = new ArrayList<>();
+//		for( int i=splitedString.length-1;i >= 0;i--) {
+//			reversedlist.add(splitedString[i]);
+//	        
+//		}
+//		
+//		
+//		  String reversedString = String.join(" ", reversedlist);
+//	        System.out.println(reversedString.trim());
+//		
+// }
+	
+	
+	
 	public static void main(String[] Args ) {
 		
 	 System.out.println(reverseWords("  the sky  is blue  "));	
