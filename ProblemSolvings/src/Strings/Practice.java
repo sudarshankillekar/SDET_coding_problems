163727512658972e11fe65b0abd9a6d5b348cdf5
sudarshan <sudarshankillekar1998@gmail.com>
2024-09-28 23:11:07 +0530

    added

--- ProblemSolvings/src/Strings/Practice.java
+++ ProblemSolvings/src/Strings/Practice.java
@@ -8,24 +8,53 @@ public class Practice {
 	
  public static void main(String[] args) {
 	
-   
-	 int arr [ ] = {1,2,3,4,5,3};
-	 int sum = 6;
+	
+	 String s1 = "silent";
+	 String s2 = "listn";
 	 
 	 
-	 for(int i = 0;i<arr.length;i++) {
-		 for(int j = i+1;j<arr.length;j++) {
-			 if(arr[i]+arr[j] == sum) {
-				 System.out.println("("+arr[i]+" "+arr[j]+")");
-			 }
-		 }
-	 }
+	 System.out.println(anagram(s1, s2));
+ }	 
+	 
+	static boolean anagram(String s1,String s2) {
+		
+	
+    
+	String str1 =	s1.replaceAll("//s", "");
+	  String str2 =  s2.replaceAll("//s", "");
+	
+	
+	if(s1.length()!=s2.length()) {
+		return false;
+	}else {
+		
+     char[] c1=  str1.toLowerCase().toCharArray();
+     char[] c2 = str2.toLowerCase().toCharArray();
+	
+   	 Arrays.sort(c1);
+   	 Arrays.sort(c2);
+   	 
+   	   return  Arrays.equals(c1, c2);
+
+	}
+	
+		
+		
+		
+	}
+	 
+	
+	
+ 
+	 
+ } 
+	
 	 
 	 
 	 
 	
- }
-}
+ 
+
  
 	
 
