08f2c385a605db18a9fea82b8f4e6d6704ca2e91
sudarshan <sudarshankillekar1998@gmail.com>
2024-10-04 23:17:45 +0530

    added

--- ProblemSolvings/src/PatternsProblem/DecreasingNumberPatterns.java
+++ ProblemSolvings/src/PatternsProblem/DecreasingNumberPatterns.java
@@ -2,13 +2,13 @@ package PatternsProblem;
 
 public class DecreasingNumberPatterns {
  
-//	123456
+//	123456d
 //	12345
 //	1234
 //	123
 //	12
 //	1
-	
+//commit 	
 	
 	public static void main(String[] args) {
 		
