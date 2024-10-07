078afbc5d19d00394711625399d17b9cdbf5a791
sudarshan <sudarshankillekar1998@gmail.com>
2024-10-08 00:43:13 +0530

    added

--- ProblemSolvings/src/Strings/Practice.java
+++ ProblemSolvings/src/Strings/Practice.java
@@ -7,15 +7,36 @@ public class Practice {
 	  
 
 	public static void main(String[] args) {
+		
+		
+	int arr[] = {1,23,4,6};
+	int target = 5;
+   System.out.println(Arrays.toString(checkfortwoSum(arr, target)));    
 	
-	String[]	strs = {"flower","flow","flight"};
-	
-          
 	
 	}
 	
 	
+	static int[] checkfortwoSum(int []nums,int target) {
+		
+	HashMap<Integer ,Integer> map = new HashMap<>();
 	
+	for(int i = 0 ;i<nums.length;i++) {
+		
+		int req_num = target - nums[i];
+		
+		if(map.containsKey(req_num)) {
+	     int  arr[] =		{map.get(req_num), i};
+	       return arr;
+		}
+		map.put(nums[i], i);
+		
+		
+	}
+		return null;
+		
+		
+	}
 	
 	
 }
