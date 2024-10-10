f7c0e40c983dc228c7e7c412caf5721a3ab12a54
sudarshan <sudarshankillekar1998@gmail.com>
2024-10-11 01:42:58 +0530

    added

--- ProblemSolvings/src/Strings/Practice.java
+++ ProblemSolvings/src/Strings/Practice.java
@@ -8,25 +8,34 @@ public class Practice {
 
 	public static void main(String[] args) {
 	
-	 
-		if(checkvowel('e')) {
-			System.out.println("is an vowel");
+	
+	int num = 19 ;
+	int count = 0;
+	
+	if(num>1) {
+		
+		for(int i = 1 ;i<=num;i++) {
+			if(num%i == 0) 
+			count++;
+			
+		}
+		
+		if(count  == 2) {
+			System.out.println("is prime");
 		}else {
-			System.out.println("is not an vowel");
+			System.out.println("is not prime");
 		}
 		
 		
-    
-    	
-    	
-    }
+	}else {
+		System.out.println("no p");
+	}
+		
+		
+	
 		
   
-	static boolean checkvowel(char ch) {
-		
-	 ch = Character.toLowerCase(ch);
-	 
-	 return ch == 'a' || ch == 'e'||ch == 'i' ||ch == 'o'||ch == 'u' ;
+	
 		
 	}
 	
