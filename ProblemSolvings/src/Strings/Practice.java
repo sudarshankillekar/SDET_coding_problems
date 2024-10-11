7400df22eec72c50904a7fd42dd013a3ae1cef17
sudarshan <sudarshankillekar1998@gmail.com>
2024-10-12 00:53:17 +0530

    added

--- ProblemSolvings/src/Strings/Practice.java
+++ ProblemSolvings/src/Strings/Practice.java
@@ -8,43 +8,57 @@ public class Practice {
 
 	public static void main(String[] args) {
 	
+        
+	int arr1[] =	{1,2,3,4,5,5,6,6,7};
+	int arr2[] =	{1,2,4,5,5,7};
+    List<Integer> intersectionOFarr = new ArrayList<>();
+    
+    intersectionOfArray(arr1, arr2, intersectionOFarr);
 	
-	int num = 19 ;
-	int count = 0;
-	
-	if(num>1) {
+   System.out.println(intersectionOFarr);
+}
+
+	static void intersectionOfArray(int []arr1,int []arr2,List<Integer>addarr) {
 		
-		for(int i = 1 ;i<=num;i++) {
-			if(num%i == 0) 
-			count++;
+		
+		int i = 0;
+		int j = 0;
+		
+		
+		while(i<arr1.length&& j<arr2.length) {
+		
+		
+			while((i<arr1.length-1) && (arr1[i] == arr1[i+1]) ) {
+				i++;
+							
+			}
+			
+			while((j<arr2.length-1) && (arr2[j] == arr2[j+1]) ) {
+				j++;					
+			}
+			
+			if(arr1[i]<arr2[j]) {
+				i++;
+			}
+			else if(arr1[i]>arr2[j]) {
+				j++;
+			}
+			
+			else {
+				
+				addarr.add(arr1[i]);
+				i++;
+				j++;
+			}
+			
 			
 		}
 		
-		if(count  == 2) {
-			System.out.println("is prime");
-		}else {
-			System.out.println("is not prime");
-		}
-		
-		
-	}else {
-		System.out.println("no p");
-	}
-		
-		
-	
-		
-  
-	
 		
 	}
-	
-
 	
 	
 }
-
-
  
   
  
