96da1e890ce0904023f773bfe776fbe66607f4dc
sudarshan <sudarshankillekar1998@gmail.com>
2024-10-14 01:59:38 +0530

    added

--- ProblemSolvings/src/Maths/FactorialOfNumber.java
+++ ProblemSolvings/src/Maths/FactorialOfNumber.java
@@ -1,23 +1,33 @@
 package Maths;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class FactorialOfNumber {
 	
 	public static void main(String[] args) {
+	
+		 int num = 5;
+		    int factorial = 1;
+		    
+		    for(int i = 1 ;i<=num;i++) {
+		    	factorial = factorial*i;
+		    }
+		    	
+		    	
+		   System.out.println(factorial);
+		    
+		  
 		
-		int num = 3;
-		
-		long factorial = 1;
-		
-		for(int i = 1;i<=num;i++) {
-			factorial = factorial * i ;
-		}
-		
-	 		
+	
+	
 		
 		
-		System.out.println(factorial);
-		
-	//	
 	}
+	
+   
+	
+	
 
 }
--- ProblemSolvings/src/Strings/Practice.java
+++ ProblemSolvings/src/Strings/Practice.java
@@ -8,57 +8,37 @@ public class Practice {
 
 	public static void main(String[] args) {
 	
-        
-	int arr1[] =	{1,2,3,4,5,5,6,6,7};
-	int arr2[] =	{1,2,4,5,5,7};
-    List<Integer> intersectionOFarr = new ArrayList<>();
-    
-    intersectionOfArray(arr1, arr2, intersectionOFarr);
+		int []arr = {1,2,3,4,5,6};
+		System.out.println(checkForSorted(arr));
+		
+		
+		}  
 	
-   System.out.println(intersectionOFarr);
-}
+	
+	static String checkForSorted(int []arr) {
+		
 
-	static void intersectionOfArray(int []arr1,int []arr2,List<Integer>addarr) {
-		
-		
-		int i = 0;
-		int j = 0;
-		
-		
-		while(i<arr1.length&& j<arr2.length) {
-		
-		
-			while((i<arr1.length-1) && (arr1[i] == arr1[i+1]) ) {
-				i++;
-							
-			}
+		for(int i = 0;i<arr.length-1;i++) {
 			
-			while((j<arr2.length-1) && (arr2[j] == arr2[j+1]) ) {
-				j++;					
-			}
-			
-			if(arr1[i]<arr2[j]) {
-				i++;
-			}
-			else if(arr1[i]>arr2[j]) {
-				j++;
-			}
-			
-			else {
-				
-				addarr.add(arr1[i]);
-				i++;
-				j++;
-			}
-			
-			
-		}
-		
-		
+			if(arr[i]>arr[i+1]) 
+				return "is not sorted";		
+		}	
+	
+	 return "is sorted";
 	}
 	
-	
 }
+	
+
+
+
+	
+		
+		
+	
+	
+	
+
  
   
  
