9b5ba089bbd1efc98be1caf1263db6f19cb07a55
sudarshan <sudarshankillekar1998@gmail.com>
2024-10-23 23:29:47 +0530

    added

--- ProblemSolvings/src/Strings/Practice.java
+++ ProblemSolvings/src/Strings/Practice.java
@@ -8,7 +8,7 @@ public class Practice {
 
 	public static void main(String[] args) {
 	
-		int []arr = {1,2,3,4,5,6};
+		int []arr = {1,2,3,4,5,6,1};
 		System.out.println(checkForSorted(arr));
 		
 		
