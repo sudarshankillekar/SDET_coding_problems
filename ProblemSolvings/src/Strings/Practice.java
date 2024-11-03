2749463b364bbc7225d04c2894f1b6bac1479abb
sudarshan <sudarshankillekar1998@gmail.com>
2024-11-03 09:43:28 +0530

    added

--- ProblemSolvings/src/Strings/Practice.java
+++ ProblemSolvings/src/Strings/Practice.java
@@ -8,12 +8,28 @@ public class Practice {
 
 	public static void main(String[] args) {
 	
+		int num = 153;
+		int rem ,sum=0;
+		int temp =num;
 		
+		while(num>0) {
+			
+			rem = num%10;
+			
+			sum = (rem*rem*rem)+sum;
+			
+			num = num/10;
+			
+		}
 		
-		
+		if(sum == temp) {
+			System.out.println("is an armstrong number");
+		}else {
+			System.out.println("is not an armstrong number");
+		}
    }
    	
-	
+
 	
 	
 }	
