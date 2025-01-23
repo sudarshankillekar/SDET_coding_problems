1ea4ef50d36956badc841267b1698adc37d547aa
sudarshan <sudarshankillekar1998@gmail.com>
2025-01-24 01:04:27 +0530

    added

--- ProblemSolvings/src/Strings/Practice.java
+++ ProblemSolvings/src/Strings/Practice.java
@@ -8,45 +8,25 @@ public class Practice {
 	
 	public static void main(String[] args) {
 	
-	 
-   ArrayList<String> tes  = new ArrayList<>();
-   tes.add("test");
-   tes.add("sudarshan");
-   tes.add("hkj");
-   tes.add("testgg");
-   
-   System.out.println(tes);
-   
-  for(String test :tes) {
-	  
-	  System.out.println(test);
-	 
-  }
-   
-   
-  HashMap<Integer, Character> internet = new HashMap<>();
-               
-  
-     internet.put(1, 'a');
-     internet.put(1, 'b');
-     
-     for (Map.Entry<Integer, Character> entry : internet.entrySet()) {
-         System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
-     }
-
-     // 2. Iterate through keys
-     for (Integer key : internet.keySet()) {
-         System.out.println("Key: " + key);
-     }
-
-     // 3. Iterate through values
-     for (Character value : internet.values()) {
-         System.out.println("Value: " + value);
-     }
-		
+	int arr [] = {12,21,23,45,22,98};
 	
-        
+	int max = Integer.MIN_VALUE;
+	int secmax = Integer.MIN_VALUE;
+	
+	for(int i = 0;i<arr.length;i++) {
 		
+		if(arr[i]>max);
+ 		max = arr[i];
+	}
+	  
+	System.out.println(max);
+     
+	for(int i = 0 ;i<arr.length;i++) {
+		if(arr[i]<max && arr[i]>secmax) {
+			secmax = arr[i];
+		}
+	}
+		System.out.println(secmax);
 		
 	}	
 		}	
